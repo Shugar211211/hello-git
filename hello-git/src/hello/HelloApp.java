d9f3b79d336a1db5c458c97e4c73bab84932cfd1
Eugeny Korobka <kashey801@gmail.com>
2021-10-12 20:46:21 +0300

    Second commit

--- hello-git/src/hello/HelloApp.java
+++ hello-git/src/hello/HelloApp.java
@@ -3,6 +3,7 @@ package hello;
 public class HelloApp {
 
 	public static void main(String[] args) {
+		// First line of code
 		System.out.println("Hello Git from Eclipse and Ubuntu 20.04 LTS !");
 	}
 
